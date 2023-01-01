216ec01fc521b3fe61a843aba20a78179e4e5f37
lawrenceuchenye <lawuche29@gmail.com>
2023-01-01 21:34:01 +0100

    we are getting there

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
+import { motion } from "framer-motion";
+import { useRef, useEffect, useState } from "react";
+import { images } from "./image_urls";
 
-import { motion } from "framer-motion"
-import { useRef,useEffect,useState } from "react"
+import "./App.css"
 
 function App() {
-  return (
-    <div className="App">
-	     <motion.h1 animate={{ x:50 }}>Hello world!</motion.h1>
-        </div>
-  );
+	return (
+		<div className="App">
+			<motion.h1 animate={{ x: 50 }}>Hello world!</motion.h1>
+			<motion.div className="carousel">
+				<motion.div className="inner-carousel">
+					{images.map((image_url) => {
+						return(
+							<motion.div className="item">
+							   <img src={image_url} alt=""/>
+							</motion.div>
+						);
+					})}
+				</motion.div>
+			</motion.div>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/image_urls.js
@@ -0,0 +1,2 @@
+
+export const images=[ "https://images.pexels.com/photos/13945391/pexels-photo-13945391.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2","https://images.pexels.com/photos/14749845/pexels-photo-14749845.jpeg?auto=compress&cs=tinysrgb&w=300&lazy=load" ];

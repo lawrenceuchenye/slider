3f1d2736813c025e2f684a01f60a5c48654332ec
lawrenceuchenye <lawuche29@gmail.com>
2023-01-01 22:19:19 +0100

    done

--- src/App.js
+++ src/App.js
@@ -2,18 +2,29 @@ import { motion } from "framer-motion";
 import { useRef, useEffect, useState } from "react";
 import { images } from "./image_urls";
 
-import "./App.css"
+import "./App.css";
 
 function App() {
+	const carousel = useRef();
+	const [width, setWidth] = useState(0);
+
+	useEffect(() => {
+		setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
+	}, []);
+
 	return (
 		<div className="App">
 			<motion.h1 animate={{ x: 50 }}>Hello world!</motion.h1>
-			<motion.div className="carousel">
-				<motion.div className="inner-carousel">
+			<motion.div ref={carousel} className="carousel">
+				<motion.div
+					drag="x"
+					dragConstraints={{ right: 0, left: -width }}
+					className="inner-carousel"
+				>
 					{images.map((image_url) => {
-						return(
-							<motion.div className="item">
-							   <img src={image_url} alt=""/>
+						return (
+							<motion.div className="item" key={image_url}>
+								<img src={image_url} alt="" key={image_url} />
 							</motion.div>
 						);
 					})}
--- src/image_urls.js
+++ src/image_urls.js
@@ -1,2 +1,2 @@
 
-export const images=[ "https://images.pexels.com/photos/13945391/pexels-photo-13945391.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2","https://images.pexels.com/photos/14749845/pexels-photo-14749845.jpeg?auto=compress&cs=tinysrgb&w=300&lazy=load" ];
+export const images=[ "https://images.pexels.com/photos/13945391/pexels-photo-13945391.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2","https://images.pexels.com/photos/14749845/pexels-photo-14749845.jpeg?auto=compress&cs=tinysrgb&w=300&lazy=load", "https://images.pexels.com/photos/10301713/pexels-photo-10301713.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" ];
